edf2755b07543db0924e668e1cb5f52d2a975b32
LR-205 <elaiarebornss@gmail.com>
2017-09-19 23:25:36 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,14 +11,14 @@ if (!$link){
 $ip = $_SERVER['REMOTE_ADDR'];
 $past = time()-150;
 mysqli_query($link, "DELETE FROM online WHERE time < ".$past);
-$result = mysqli_query("SELECT time FROM online WHERE ip='".$ip."'");
+$result = mysqli_query($link, "SELECT time FROM online WHERE ip='$ip'");
 $time = time();
 if($row = mysqli_fetch_assoc($result)){
-    mysqli_query("UPDATE online SET time='$time',ip='$ip' WHERE ip='$ip'");
+    mysqli_query($link, "UPDATE online SET time='$time',ip='$ip' WHERE ip='$ip'");
 }else{
-    mysqli_query("INSERT INTO online (ip,time) VALUES ('$ip','$time')") or die(mysql_error());
+    mysqli_query($link, "INSERT INTO online (ip,time) VALUES ('$ip','$time')") or die(mysqli_error());
 }
-$result   = mysqli_query("SELECT ip FROM online");
+$result   = mysqli_query($link, "SELECT ip FROM online");
 $aktifkac = mysqli_num_rows($result);
 #---Aktif Ziyaretçi Sayısı--------------------------------------------------------------------
 
@@ -29,14 +29,14 @@ $year = substr($baslat, 0,4);
 $month = substr($baslat, 5, 2);
 $day = substr($baslat, 8, 2);
 $bitis = date("Y-m-d", mktime(0, 0, 0, $month, $day-1, $year));
-$sorgula = mysqli_query("select tarih from ziyaret where tarih='$bitis'");
+$sorgula = mysqli_query($link, "select tarih from ziyaret where tarih='$bitis'");
 $dunku = mysqli_num_rows($sorgula);
 #---Dün Tekil Toplam Kaç Kişi Girmiş----------------------------------------------------------
 
 
 #---Bugün Tekil Toplam Kaç Kişi Girmiş--------------------------------------------------------
 $bugun = date("Y-m-d");
-$sorgu = mysqli_query("select tarih from ziyaret where tarih='$bugun'");
+$sorgu = mysqli_query($link, "select tarih from ziyaret where tarih='$bugun'");
 $bugunku = mysqli_num_rows($sorgu);
 #---Bugün Tekil Toplam Kaç Kişi Girmiş--------------------------------------------------------
 
@@ -45,20 +45,20 @@ $bugunku = mysqli_num_rows($sorgu);
 $ipsi  = $_SERVER['REMOTE_ADDR'];
 $tarih = date("Y-m-d");
 
-$ipkontrol = mysqli_query("select * from ziyaret where ip='$ipsi' order by id desc");
+$ipkontrol = mysqli_query($link, "select * from ziyaret where ip='$ipsi' order by id desc");
 $yaz   = mysqli_fetch_assoc($ipkontrol);
 $vip   = $yaz['ip'];
 $vtarih= $yaz['tarih'];
 $bak = mysqli_num_rows($ipkontrol);
 if($bak>0){  //if-
     if($vtarih<$tarih){
-        $kayit_1 = mysqli_query("insert into ziyaret (ip,tarih) values ('$ipsi','$tarih')");
+        $kayit_1 = mysqli_query($link, "insert into ziyaret (ip,tarih) values ('$ipsi','$tarih')");
     }
 }//if-
 else{
-    $kayit_2 = mysqli_query("insert into ziyaret (ip,tarih) values ('$ipsi','$tarih')");
+    $kayit_2 = mysqli_query($link, "insert into ziyaret (ip,tarih) values ('$ipsi','$tarih')");
 }
-$toplamne = mysqli_query("select * from ziyaret");
+$toplamne = mysqli_query($link, "select * from ziyaret");
 $toplamziyaret  = mysqli_num_rows($toplamne);
 #---Toplam Tekil Kaç Kişi Girmiş--------------------------------------------------------------
 ?>
